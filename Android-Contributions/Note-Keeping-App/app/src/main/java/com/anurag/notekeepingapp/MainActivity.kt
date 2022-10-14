edc017048d4109a55a5b27819c0be59f279d5eff
Amandeep Singh <contactamandeep18@gmail.com>
2022-10-14 20:00:00 +0530

    EditText is cleared when a new note is added

--- Android-Contributions/Note-Keeping-App/app/src/main/java/com/anurag/notekeepingapp/MainActivity.kt
+++ Android-Contributions/Note-Keeping-App/app/src/main/java/com/anurag/notekeepingapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.anurag.notekeepingapp
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -32,7 +33,9 @@ class MainActivity : AppCompatActivity(), OnTapHandler {
     }
 
   override fun alertDialog(position: NoteEntity){
+
      val dialog = AlertDialog.Builder(this)
+
 
      dialog.setTitle(getString(R.string.alert_dialog_title))
      dialog.setIcon(R.drawable.ic_round_error_outline_24)
@@ -54,7 +57,10 @@ class MainActivity : AppCompatActivity(), OnTapHandler {
         val noteText = binding.addNoteView.text.toString()
         if (noteText.isNotEmpty()) {
             viewModel.insert(NoteEntity(note = noteText))
+            binding.addNoteView.text.clear()
         }
+
+
     }
 
 }
